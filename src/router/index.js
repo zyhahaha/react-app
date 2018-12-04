7cb09417f3f31f3ae29476b05ed2f8bac16b491f
zhaoyang <18355403288@163.com>
2018-12-04 21:49:16 +0800

    people

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import asyncComponent from '@/utils/asyncComponent';
 
 import home from "@/pages/home/home";
-const about = asyncComponent(() => import("@/pages/about/about"));
+const people = asyncComponent(() => import("@/pages/people/people"));
 // const helpcenter = asyncComponent(() => import("@/pages/helpcenter/helpcenter"));
 // const production = asyncComponent(() => import("@/pages/production/production"));
 // const balance = asyncComponent(() => import("@/pages/balance/balance"));
@@ -15,7 +15,7 @@ export default class RouteConfig extends Component{
       <HashRouter>
         <Switch>
           <Route path="/" exact component={home} />
-          <Route path="/about" exact component={about} />
+          <Route path="/people" exact component={people} />
           {/* <Route path="/record" component={record} />
           <Route path="/helpcenter" component={helpcenter} />
           <Route path="/production" component={production} />
