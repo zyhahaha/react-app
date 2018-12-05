2611ba38aec26eb6105f8d35866d2080bf476ee7
zhaoyang <18355403288@163.com>
2018-12-05 22:08:14 +0800

    menu

---
+++ src/pages/menu/menu.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './menu.less';
+
+import noticeImg from '@/assets/imgs/menu/notice.png';
+import collectImg from '@/assets/imgs/menu/collect.png';
+
+class Menu extends Component {
+  render() {
+    return (
+      <div className="menu">
+          <div className="band_card">
+            <p className="avatar_wrap">
+              <img src="" alt=""/>
+            </p>
+            <p className="nickname">木棍</p>
+            <p className="summary">与更好的自己相遇。</p>
+          </div>
+          <div className="item_wrap">
+            <ul>
+              <li>
+                <img src={noticeImg} alt=""/>
+                <span>我的消息</span>
+              </li>
+              <li>
+                <img src={collectImg} alt=""/>
+                <span>我的收藏</span>
+              </li>
+            </ul>
+          </div>
+      </div>
+    )
+  }
+}
+
+export default Menu;
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import asyncComponent from '@/utils/asyncComponent';
 
-// import home from "@/pages/home/home";
+import home from "@/pages/home/home";
 const people = asyncComponent(() => import("@/pages/people/people"));
 const MoreCourse = asyncComponent(() => import("@/pages/more_course/more_course"));
 const Avatar = asyncComponent(() => import("@/pages/avatar/avatar"));
 const Avatar1 = asyncComponent(() => import("@/pages/avatar.1/avatar"));
-// const balance = asyncComponent(() => import("@/pages/balance/balance"));
+const menu = asyncComponent(() => import("@/pages/menu/menu"));
 
 // react-router4 不再推荐将所有路由规则放在同一个地方集中式路由，子路由应该由父组件动态配置，组件在哪里匹配就在哪里渲染，更加灵活
 export default class RouteConfig extends Component{
@@ -15,11 +15,12 @@ export default class RouteConfig extends Component{
     return(
       <HashRouter>
         <Switch>
-          {/* <Route path="/" exact component={home} /> */}
+          <Route path="/" exact component={home} />
           <Route path="/people" exact component={people} />
           <Route path="/more_course" component={MoreCourse} />
           <Route path="/avatar" component={Avatar} />
           <Route path="/avatar1" component={Avatar1} />
+          <Route path="/menu" component={menu} />
           <Redirect to="/" />
         </Switch>
       </HashRouter>
